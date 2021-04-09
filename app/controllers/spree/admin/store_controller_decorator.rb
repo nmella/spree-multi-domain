55519fafdb3178fe2483ae4d9603a8367c388e49
Rahul <rahul@bluebash.co>
2021-04-09 17:10:54 +0530

    fixed minor issue of loading for store controller

--- app/controllers/spree/admin/store_controller_decorator.rb
+++ app/controllers/spree/admin/store_controller_decorator.rb
@@ -1,24 +1,38 @@
-class Spree::Admin::StoresController < Spree::Admin::ResourceController
-  before_action :load_payment_methods
-  before_action :load_shipping_methods
+module Spree
+  module Admin
+    module StoresControllerDecorator
+    def self.prepended(base)
+      base.before_action :load_payment_methods
+      base.before_action :load_shipping_methods
+    end
 
-  def index
-    @stores = @stores.ransack({ name_or_domains_or_code_cont: params[:q] }).result if params[:q]
-    @stores = @stores.where(id: params[:ids].split(',')) if params[:ids]
+    def index
+      @stores = Spree::Store.all
+      @stores = @stores.ransack({ name_or_domains_or_code_cont: params[:q] }).result if params[:q]
+      @stores = @stores.where(id: params[:ids].split(',')) if params[:ids]
 
-    respond_with(@stores) do |format|
-      format.html
-      format.json
+      respond_with(@stores) do |format|
+        format.html
+        format.json
+      end
+    end
+
+    protected
+
+    def permitted_store_params
+      params.require(:store).permit!
+    end
+
+    private
+      def load_payment_methods
+        @payment_methods = Spree::PaymentMethod.all
+      end
+
+      def load_shipping_methods
+        @shipping_methods = Spree::ShippingMethod.all
+      end
     end
   end
-
-  private
-
-  def load_payment_methods
-    @payment_methods = Spree::PaymentMethod.all
-  end
-
-  def load_shipping_methods
-    @shipping_methods = Spree::ShippingMethod.all
-  end
 end
+
+::Spree::Admin::StoresController.prepend Spree::Admin::StoresControllerDecorator
